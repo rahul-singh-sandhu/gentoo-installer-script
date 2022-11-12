d3ee75b1154ecf2db9f78b43b815af7587f2e57b
Rahul Sandhu <rahul@sandhuservices.dev>
2022-11-12 17:21:45 +0000

    Added installer part 2, refractoring.

---
+++ gentoo-install-part2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# _____   ______
+# |  __ \ / ____| Rahul Sandhu
+# | |__) | (___   rahul@sandhuservices.dev
+# |  _  / \___ \  https://sandhuservices.dev/
+# | | \ \ ____) | https://gitlab.sandhuservices.dev/rahulsandhu/
+# |_|  \_\_____/  https://github.com/rahul-singh-sandhu
+
+NAME="Gentoo Installer"
+CODENAME="gentooinstaller"
+COPYRIGHT="Copyright (C) 2022 Rahul Sandhu"
+LICENSE="GNU General Public License 3.0"
+VERSION="0.1"
--- gentoo-install.sh
+++ gentoo-install.sh
@@ -13,6 +13,8 @@ COPYRIGHT="Copyright (C) 2022 Rahul Sandhu"
 LICENSE="GNU General Public License 3.0"
 VERSION="0.1"
 
+INSTALLER_SRC_DIR=$(pwd)
+
 lsblk
 read -p 'Enter the drive name: ' drive
 read -p 'Would you like to use swap? (Y/n) ' use_swap_temp
@@ -33,14 +35,14 @@ case $use_swap in
 esac
 
 echo $use_swap_final
-if [[ $use_swap == 'yes' ]] || [[ $use_swap == 'y' ]]
+if [[ $use_swap_final == 1 ]]
 then
 	read -p 'Enter a size for swap space in GB ' swap_space_temp
 	swap_space=$(echo -e "$swap_space_temp" | sed 's/[^0-9]*//g')
 	echo $swap_space
 fi
 
-if [[ $use_luks == 'yes' ]] || [[ $use_luks == 'y' ]]
+if [[ $use_luks_final == 1 ]]
 then
         read -p 'Enter your luks keyphrase ' luks_password
 fi
@@ -48,15 +50,15 @@ fi
 check_drive_type() {
 	if [[ $drive == *"nvme"* ]]
 	then
-		nvme=true
+		nvme=1
 	else
-		nvme=false
+		nvme=0
 	fi
 }
 
 partition_drives() {
 	sgdisk -Z $drive && wipefs -a $drive
-	if [[ $use_swap == 'yes' ]] || [[ $use_swap == 'y' ]]
+	if [[ $use_swap_final == 1 ]]
 	then
 		sgdisk -o -n 1::+500M -t 1:EF00 -c 1:"boot" -n 2::+${swap_space}gb -t 2:8200 -c 2:"swap" -n 3::: -t 3:8300 -c 3:"root" -p $drive
 	else
@@ -65,14 +67,14 @@ partition_drives() {
 }
 format_drives() {
 	mkdir -pv /mnt/gentoo
-	if [[ $nvme == true ]]
+	if [[ $nvme == 1 ]]
 	then
 		mkfs.vfat -F32 ${drive}p1
-		if [[ $use_swap == 'yes' ]] || [[ $use_swap == 'y' ]]
+		if [[ $use_swap_final == 1 ]]
 		then
 			mkswap ${drive}p2
 			swapon ${drive}p2
-			if [[ $use_luks == 'yes' ]] || [ $use_luks == 'y' ]
+			if [[ $use_luks_final == 1 ]]
 			then
 				echo -en $luks_password | cryptsetup -q luksFormat ${drive}p3
 				echo -en $luks_password | cryptsetup open ${drive}p3 gentoolukstest
@@ -85,7 +87,7 @@ format_drives() {
 				btrfs_drive_path="$drive"p3
 			fi
 		else
-			if [[ $use_luks == 'yes' ]] || [ $use_luks == 'y' ]
+			if [[ $use_luks_final == 1 ]]
 			then
 				echo -en $luks_password | cryptsetup -q luksFormat ${drive}p2
 				echo -en $luks_password | cryptsetup open ${drive}p2 gentoolukstest
@@ -100,11 +102,11 @@ format_drives() {
 		fi
 	else
 		mkfs.vfat -F32 ${drive}1
-		if [[ $use_swap == 'yes' ]] || [[ $use_swap == 'y' ]]
+		if [[ $use_swap_final == 1 ]]
         then
             mkswap ${drive}2
             swapon ${drive}2
-            if [[ $use_luks == 'yes' ]] || [ $use_luks == 'y' ]
+            if [[ $use_luks_final == 1 ]]
             then
             	echo -en $luks_password | cryptsetup -q luksFormat ${drive}3
                 echo -en $luks_password | cryptsetup open ${drive}3 gentoolukstest
@@ -117,7 +119,7 @@ format_drives() {
 				btrfs_drive_mount="$drive"3
             fi
 		else
-			if [[ $use_luks == 'yes' ]] || [ $use_luks == 'y' ]
+			if [[ $use_luks_final == 1 ]] 
 			then
 				echo -en $luks_password | cryptsetup -q luksFormat ${drive}2
 				echo -en $luks_password | cryptsetup open ${drive}2 gentoolukstest
@@ -154,11 +156,41 @@ fetch_stage3() {
 configure_stage3() {
 	sed -i 's/COMMON_FLAGS="/&-march=native /' /mnt/gentoo/etc/portage/make.conf
 	sed -i "/FFLAGS/aMAKEOPTS=\"${user_make_opts}\"" /mnt/gentoo/etc/portage/make.conf
-
+	mkdir --parents /mnt/gentoo/etc/portage/repos.conf
+	cp /mnt/gentoo/usr/share/portage/config/repos.conf /mnt/gentoo/etc/portage/repos.conf/gentoo.conf
+	cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
 }
 
-chroot_install() {
-	echo hello
+chroot_mount_fs() {
+	mount --types proc /proc /mnt/gentoo/proc
+	mount --rbind /sys /mnt/gentoo/sys 
+	mount --make-rslave /mnt/gentoo/sys
+	mount --rbind /dev /mnt/gentoo/dev 
+	mount --make-rslave /mnt/gentoo/dev 
+	mount --bind /run /mnt/gentoo/run 
+	mount --make-slave /mnt/gentoo/run 
+	test -L /dev/shm && rm /dev/shm && mkdir /dev/shm
+	mount --types tmpfs --options nosuid,nodev,noexec shm /dev/shm 
+	chmod 1777 /dev/shm /run/shm
+}
+
+mount_volumes() {
+	mount -o noatime,nodiratime,compress=zstd:1,ssd,subvol=@home ${btrfs_drive_path} /mnt/gentoo/home
+	if [[ $nvme = 1 ]]
+	then
+		mount ${drive}p1 /mnt/gentoo/boot
+	else
+		mount ${drive}1 /mnt/gentoo/boot
+	fi 
+}
+
+chroot_first_run() {
+	cp ${INSTALLER_SRC_DIR}/gentoo-install-part2.sh /mnt/gentoo/root/gentoo-install-part2.sh
+	chroot /mnt/gentoo /bin/bash 
+	chroot source /etc/profile 
+	chroot export PS1="(chroot) ${PS1}"
+	chroot chmod +x /root/gentoo-install-part2.sh
+	chroot /bin/bash /root/gentoo-install-part2.sh
 }
 
 check_drive_type
@@ -166,3 +198,7 @@ partition_drives
 format_drives
 create_subvols
 fetch_stage3
+configure_stage3
+chroot_mount_fs
+mount_volumes
+chroot_first_run
